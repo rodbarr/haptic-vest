f4f71cdbee2d1af0fbc49cab4eabdc2326c14d09
Rodrigo Barrientos <rodrigobarrientos@Rodrigos-MacBook-Pro.local>
2020-11-21 22:43:12 -0600

    Fisrt push

---
+++ Assets/HitMarker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HM : MonoBehaviour
+{
+    public enum Hitmrkr{Body, Head}
+    public Hitmrkr hitmarkerttype;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(hitmarkerttype == Hitmrkr.Body && other.gameObject.tag =="bullet")
+        {
+            Health.instance.maxHealth -= 10;
+        }
+        else if (hitmarkerttype == Hitmrkr.Head && other.gameObject.tag == "bullet")
+        {
+            Health.instance.maxHealth -= 10;
+        }
+    }
+
+}
---
+++ Assets/WEAPONRAYCAST.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class WEAPONRAYCAST : MonoBehaviour
+{
+    private GameObject raycastedObj;
+
+    [SerializeField] private int raylenght = 100;
+    [SerializeField] private LayerMask layerMaskInteract;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, raylenght,layerMaskInteract.value))
+        {
+            if (hit.collider.CompareTag("BOX"))
+            {
+                raycastedObj = hit.collider.gameObject;
+
+                if (Input.GetKeyDown("mouse 0"))
+                {
+                    Debug.Log("I HAVE INTERACTED WITH AN OBJECT");
+                }
+            }
+        }
+
+    }
+   
+}
